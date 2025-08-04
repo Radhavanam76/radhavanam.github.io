13a32737a2942e8f1d13ded31fa7d66c7b4c7cb5
Radhavanam <13024C055@GMAIL.COM>
2025-08-04 23:38:53 +0530

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,3 +1,6 @@
+// scripts/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+
 export const firebaseConfig = {
   apiKey: "AIzaSyAOjNNbZicYA0BdWxrJeaKo6a8-97B0edQ",
   authDomain: "radhavanam-76.firebaseapp.com",
@@ -7,3 +10,5 @@ export const firebaseConfig = {
   appId: "1:39126946397:web:69bcfb4eb0bbc31a4733b4",
   measurementId: "G-Q9R5M417CJ"
 };
+
+export const app = initializeApp(firebaseConfig);
